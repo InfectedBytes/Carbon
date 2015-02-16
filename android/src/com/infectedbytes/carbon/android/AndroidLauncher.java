d6a79be9a0eefeca08f74bc2f73d3ee7857fa1c3
Henrik Heine <dev@infectedbytes.com>
2015-02-16 23:42:59 +0100

    Initial projects
    Added initial projects

---
+++ android/src/com/infectedbytes/carbon/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.infectedbytes.carbon.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.infectedbytes.carbon.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
---
+++ desktop/src/com/infectedbytes/carbon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.infectedbytes.carbon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.infectedbytes.carbon.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
